8914c0389e1b092917e588f8177a009934cab400
I078235 <sally.weng@sap.com>
2018-12-06 17:33:17 +0800

    schedule

--- src/main/java/com/successfactors/sfmooc/dao/impl/UserDAOImpl.java
+++ src/main/java/com/successfactors/sfmooc/dao/impl/UserDAOImpl.java
@@ -40,22 +40,7 @@ public class UserDAOImpl implements UserDAO {
     public User getUserById(String userId) {
         String query = "select id, nickname, avatarUrl, status, department, signature, seat, " +
                 "blog, github from user where id = ?";
-        List<User> users = jdbcTemplate.query(query, new Object[]{userId}, new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                User user = new User();
-                user.setId(resultSet.getString("id"));
-                user.setNickName(resultSet.getString("nickname"));
-                user.setAvatarUrl(resultSet.getString("avatarUrl"));
-                user.setStatus(resultSet.getInt("status"));
-                user.setDepartment(resultSet.getString("department"));
-                user.setSignature(resultSet.getString("signature"));
-                user.setSeat(resultSet.getString("seat"));
-                user.setBlog(resultSet.getString("blog"));
-                user.setGithub(resultSet.getString("github"));
-                return user;
-            }
-        });
+        List<User> users = jdbcTemplate.query(query, new Object[]{userId}, new UserRowMapper());
         if(users != null && !users.isEmpty()){
             return users.get(0);
         }
@@ -65,22 +50,25 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public List<User> getUsersByOrder() {
         String query = "select * from user where `order` is not null order by `order` asc " ;
-        List<User> users = jdbcTemplate.query(query, new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                User user = new User();
-                user.setId(resultSet.getString("id"));
-                user.setNickName(resultSet.getString("nickname"));
-                user.setAvatarUrl(resultSet.getString("avatarUrl"));
-                user.setStatus(resultSet.getInt("status"));
-                user.setDepartment(resultSet.getString("department"));
-                user.setSignature(resultSet.getString("signature"));
-                user.setSeat(resultSet.getString("seat"));
-                user.setBlog(resultSet.getString("blog"));
-                user.setGithub(resultSet.getString("github"));
-                return user;
-            }
-        });
+        List<User> users = jdbcTemplate.query(query, new UserRowMapper());
         return users;
     }
+
+    class UserRowMapper implements RowMapper<User> {
+        @Override
+        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+            User user = new User();
+            user.setId(resultSet.getString("id"));
+            user.setNickName(resultSet.getString("nickname"));
+            user.setAvatarUrl(resultSet.getString("avatarUrl"));
+            user.setStatus(resultSet.getInt("status"));
+            user.setDepartment(resultSet.getString("department"));
+            user.setSignature(resultSet.getString("signature"));
+            user.setSeat(resultSet.getString("seat"));
+            user.setBlog(resultSet.getString("blog"));
+            user.setGithub(resultSet.getString("github"));
+            return  user;
+        }
+    }
+
 }
