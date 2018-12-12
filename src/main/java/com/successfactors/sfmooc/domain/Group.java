b40e0cc45a18e726341dc63841e11a2a5f301f39
I078235 <sally.weng@sap.com>
2018-12-12 17:11:55 +0800

    support sessionType

---
+++ src/main/java/com/successfactors/sfmooc/domain/Group.java
@@ -0,0 +1,25 @@
+package com.successfactors.sfmooc.domain;
+
+public class Group {
+    private Integer id;
+    private String name;
+
+    public Group( ) {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
